6e90e60c7c8bbdb330b4bb9ba56c19942582471e
bjutxe <cube4971@gmail.com>
2024-07-19 12:48:56 +0900

    fix

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-// const isSP = navigator.userAgent.match(/iPhone|Android.+Mobile/);
-const isSP = true;
+const isSP = navigator.userAgent.match(/iPhone|Android.+Mobile/);
 
 const BLOCK_SIZE = isSP ? 30 : window.screen.availHeight > 912 ? 80 : 70;
 const [FIELD_X, FIELD_Y] = isSP ? [11, 20] : [20, 11];
 const [START_HEAD_X, START_HEAD_Y] = isSP ? [0, 4] : [4, 10];
 const MARGIN = isSP ? 3 : 10;
 
-const SPEED = 1000 / 1.6; //1.6OK 1.7NG
+const SPEED = 1000 / 1.6;
 
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
@@ -19,7 +18,7 @@ const operation = [],
 var pickStar = [];
 const regexWasd = /Key(W|A|S|D)/;
 const regexArrow = /Arrow(Left|Right|Up|Down)/;
-const regexOther = /Key(P|R|B)/;
+const regexOther = /Key(P|R)/;
 
 var loopCounter = 0;
 var isPaused = false;
@@ -41,14 +40,6 @@ const snake = {
       this.x += this.dx;
       this.y += this.dy;
     }
-    const address = this.y * FIELD_X + this.x;
-    console.log(
-      String(address) + " : " + String(this.x) + ", " + String(this.y)
-    );
-    const dead = pickStar.findIndex((item) => item == address);
-    console.log("dead: ", dead);
-    console.log("pickStar[dead]: ", pickStar[dead]);
-    if (dead >= 0) pickStar.splice(dead, 1);
   },
 };
 
@@ -60,12 +51,6 @@ const init = () => {
   snake.len = 5;
   operation.length = 0;
   nextHead.length = 0;
-  pickStar.length = 0;
-  for (let row = 0; row < FIELD_Y; row++) {
-    for (let col = 0; col < FIELD_X; col++) {
-      pickStar.push(row * FIELD_X + col);
-    }
-  }
   isSP ? initSP() : initPC();
   paint();
   loopCounter = 0;
@@ -82,7 +67,6 @@ const initPC = () => {
   snake.dy = 0;
   star.x = 10;
   star.y = 5;
-  pickStar.splice(snake.body[0].y * FIELD_X + snake.body[0].x, 5);
 };
 
 const initSP = () => {
@@ -96,294 +80,47 @@ const initSP = () => {
   snake.dy = 1;
   star.x = 5;
   star.y = 10;
-  pickStar.splice(snake.y * FIELD_X + snake.x, 1);
-  snake.body.forEach((it) => {
-    const address = it.y * FIELD_X + it.x;
-    const dead = pickStar.findIndex((item) => item == address);
-    pickStar.splice(dead, 1);
-  });
-};
-
-const debugInitSP = () => {
-  snake.x = 0;
-  snake.y = 16;
-  snake.len = 141;
-  operation.length = 0;
-  nextHead.length = 0;
-  loopCounter = 0;
-  snake.body = [
-    { x: 8, y: 0 },
-    { x: 9, y: 0 },
-    { x: 10, y: 0 },
-    { x: 10, y: 1 },
-    { x: 9, y: 1 },
-    { x: 8, y: 1 },
-    { x: 7, y: 1 },
-    { x: 6, y: 1 },
-    { x: 5, y: 1 },
-    { x: 4, y: 1 },
-    { x: 3, y: 1 },
-    { x: 2, y: 1 },
-    { x: 1, y: 1 },
-    { x: 1, y: 2 },
-    { x: 2, y: 2 },
-    { x: 3, y: 2 },
-    { x: 4, y: 2 },
-    { x: 5, y: 2 },
-    { x: 6, y: 2 },
-    { x: 7, y: 2 },
-    { x: 8, y: 2 },
-    { x: 9, y: 2 },
-    { x: 10, y: 2 },
-    { x: 10, y: 3 },
-    { x: 9, y: 3 },
-    { x: 8, y: 3 },
-    { x: 7, y: 3 },
-    { x: 6, y: 3 },
-    { x: 5, y: 3 },
-    { x: 4, y: 3 },
-    { x: 3, y: 3 },
-    { x: 2, y: 3 },
-    { x: 1, y: 3 },
-    { x: 1, y: 4 },
-    { x: 2, y: 4 },
-    { x: 3, y: 4 },
-    { x: 4, y: 4 },
-    { x: 5, y: 4 },
-    { x: 6, y: 4 },
-    { x: 7, y: 4 },
-    { x: 8, y: 4 },
-    { x: 9, y: 4 },
-    { x: 10, y: 4 },
-    { x: 10, y: 5 },
-    { x: 10, y: 6 },
-    { x: 10, y: 7 },
-    { x: 10, y: 8 },
-    { x: 10, y: 9 },
-    { x: 10, y: 10 },
-    { x: 10, y: 11 },
-    { x: 10, y: 12 },
-    { x: 10, y: 13 },
-    { x: 10, y: 14 },
-    { x: 10, y: 15 },
-    { x: 10, y: 16 },
-    { x: 10, y: 17 },
-    { x: 10, y: 18 },
-    { x: 10, y: 19 },
-    { x: 9, y: 19 },
-    { x: 8, y: 19 },
-    { x: 7, y: 19 },
-    { x: 6, y: 19 },
-    { x: 5, y: 19 },
-    { x: 4, y: 19 },
-    { x: 3, y: 19 },
-    { x: 2, y: 19 },
-    { x: 1, y: 19 },
-    { x: 0, y: 19 },
-    { x: 0, y: 18 },
-    { x: 1, y: 18 },
-    { x: 2, y: 18 },
-    { x: 3, y: 18 },
-    { x: 4, y: 18 },
-    { x: 5, y: 18 },
-    { x: 6, y: 18 },
-    { x: 7, y: 18 },
-    { x: 8, y: 18 },
-    { x: 9, y: 18 },
-    { x: 9, y: 17 },
-    { x: 9, y: 16 },
-    { x: 9, y: 15 },
-    { x: 9, y: 14 },
-    { x: 9, y: 13 },
-    { x: 9, y: 12 },
-    { x: 9, y: 11 },
-    { x: 9, y: 10 },
-    { x: 9, y: 9 },
-    { x: 9, y: 8 },
-    { x: 9, y: 7 },
-    { x: 9, y: 6 },
-    { x: 9, y: 5 },
-    { x: 8, y: 5 },
-    { x: 7, y: 5 },
-    { x: 6, y: 5 },
-    { x: 5, y: 5 },
-    { x: 4, y: 5 },
-    { x: 3, y: 5 },
-    { x: 2, y: 5 },
-    { x: 1, y: 5 },
-    { x: 1, y: 6 },
-    { x: 2, y: 6 },
-    { x: 3, y: 6 },
-    { x: 4, y: 6 },
-    { x: 5, y: 6 },
-    { x: 6, y: 6 },
-    { x: 7, y: 6 },
-    { x: 8, y: 6 },
-    { x: 8, y: 7 },
-    { x: 7, y: 7 },
-    { x: 6, y: 7 },
-    { x: 5, y: 7 },
-    { x: 4, y: 7 },
-    { x: 3, y: 7 },
-    { x: 2, y: 7 },
-    { x: 1, y: 7 },
-    { x: 1, y: 8 },
-    { x: 2, y: 8 },
-    { x: 3, y: 8 },
-    { x: 4, y: 8 },
-    { x: 5, y: 8 },
-    { x: 6, y: 8 },
-    { x: 7, y: 8 },
-    { x: 8, y: 8 },
-    { x: 8, y: 9 },
-    { x: 8, y: 10 },
-    { x: 8, y: 11 },
-    { x: 8, y: 12 },
-    { x: 8, y: 13 },
-    { x: 8, y: 14 },
-    { x: 8, y: 15 },
-    { x: 8, y: 16 },
-    { x: 8, y: 17 },
-    { x: 7, y: 17 },
-    { x: 6, y: 17 },
-    { x: 5, y: 17 },
-    { x: 4, y: 17 },
-    { x: 3, y: 17 },
-    { x: 2, y: 17 },
-    { x: 1, y: 17 },
-    { x: 0, y: 17 },
-  ];
-  snake.dx = 1;
-  snake.dy = 0;
-  star.x = 1;
-  star.y = 16;
-  pickStar = [
-    192, 177, 178, 179, 180, 181, 182, 183, 172, 171, 170, 169, 168, 167, 166,
-    165, 154, 155, 156, 157, 158, 159, 160, 161, 139, 128, 117, 106, 105, 116,
-    127, 138, 149, 148, 147, 146, 145, 144, 143, 132, 133, 134, 135, 136, 137,
-    126, 125, 124, 123, 122, 121, 110, 111, 112, 113, 114, 115, 104, 103, 102,
-    101, 100, 99, 88, 77, 66, 55, 44, 33, 22, 11, 0, 1, 2, 3, 4, 5, 6, 7,
-  ];
-  paint();
 };
 
 const loop = () => {
-  var newSetSnakeBody = new Set(pickStar);
-  newSetSnakeBody.add(snake.y * FIELD_X + snake.x);
-  snake.body.forEach((it) => {
-    newSetSnakeBody.add(it.y * FIELD_X + it.x);
-  });
-  console.log("newSetSnakeBody.size: ", newSetSnakeBody.size);
-  if (newSetSnakeBody.size != 220) {
-    isPaused = true;
-    for (let i = 0; i < 220; i++) {
-      if (!newSetSnakeBody.has(i)) {
-        console.log("miss: ", i);
-      }
-    }
-  }
-  if (snake.len + pickStar.length !== 220) {
-    console.log("not 220: " + (snake.len + pickStar.length) + "!!!");
-    console.log("snake.len: ", snake.len);
-    console.log("pickStar.length: ", pickStar.length);
-    isPaused = true;
-  }
   loopCounter++;
   snake.move();
   const eaten = snake.x === star.x && snake.y === star.y;
   if (eaten) snake.len++;
-  if (snake.body.length >= snake.len) {
-    const useful = snake.body.shift();
-    console.log("useful: ", useful);
-    var pushIdx = useful.y * FIELD_X + useful.x;
-    console.log("pushIdx: ", pushIdx);
-    pickStar.push(pushIdx);
-  }
-  var newSetSnakeBody = new Set(pickStar);
-  newSetSnakeBody.add(snake.y * FIELD_X + snake.x);
-  snake.body.forEach((it) => {
-    newSetSnakeBody.add(it.y * FIELD_X + it.x);
-  });
-  console.log("newSetSnakeBody.size: ", newSetSnakeBody.size);
-  if (newSetSnakeBody.size != 220) {
-    isPaused = true;
-    for (let i = 0; i < 220; i++) {
-      if (!newSetSnakeBody.has(i)) {
-        console.log("miss: ", i);
-      }
-    }
-  }
-  debuglog();
+  if (snake.body.length >= snake.len) snake.body.shift();
   if (eaten) {
-    console.log("EATEN!!!");
-    var randomNum = Math.random();
-    console.log("randomNum: ", randomNum);
-    console.log("randomNum: ", randomNum);
-    var randomStar = randomNum * pickStar.length;
-    console.log("randomNum: ", randomNum);
-    console.log("pickStar.length: ", pickStar.length);
-    console.log("randomStar: ", randomStar);
-    console.log(
-      String(randomNum.toFixed(6)) +
-        " * " +
-        String(pickStar.length) +
-        " = " +
-        String(randomStar)
-    );
-    var floorStar = Math.floor(randomStar);
-    console.log("floorStar: ", floorStar);
-    var nowPickStar = pickStar[floorStar];
-    console.log("nowPickStar: ", nowPickStar);
-    const nextStar = nowPickStar;
-    console.log("nextStar: ", nextStar);
-    star.x = nextStar % FIELD_X;
-    console.log("FIELD_X: ", FIELD_X);
-    console.log(String(nextStar) + " % " + String(FIELD_X) + " = " + star.x);
-    star.y = Math.floor(nextStar / FIELD_X);
-    console.log(
-      "Math.floor(" +
-        String(nextStar) +
-        " / " +
-        String(FIELD_X) +
-        ") = " +
-        star.y
-    );
-    console.log("star.x, star.y: ", star.x, star.y);
-  }
-  var newSetSnakeBody = new Set(pickStar);
-  newSetSnakeBody.add(snake.y * FIELD_X + snake.x);
-  snake.body.forEach((it) => {
-    newSetSnakeBody.add(it.y * FIELD_X + it.x);
-  });
-  console.log("newSetSnakeBody.size: ", newSetSnakeBody.size);
-  if (newSetSnakeBody.size != 220) {
-    isPaused = true;
-    for (let i = 0; i < 220; i++) {
-      if (!newSetSnakeBody.has(i)) {
-        console.log("miss: ", i);
+    pickStar.length = 0;
+    for (let row = 0; row < FIELD_Y; row++) {
+      for (let col = 0; col < FIELD_X; col++) {
+        pickStar.push(row * FIELD_X + col);
       }
     }
+    let address = snake.y * FIELD_X + snake.x;
+    let dead = pickStar.findIndex((item) => item == address);
+    pickStar.splice(dead, 1);
+    snake.body.forEach((it) => {
+      address = it.y * FIELD_X + it.x;
+      dead = pickStar.findIndex((item) => item == address);
+      pickStar.splice(dead, 1);
+    });
+    var nowPickStar = pickStar[Math.floor(Math.random() * pickStar.length)];
+    const nextStar = nowPickStar;
+    star.x = nextStar % FIELD_X;
+    star.y = Math.floor(nextStar / FIELD_X);
   }
   snake.body.forEach((it) => {
     if (snake.x === it.x && snake.y === it.y) {
       console.log("head hit body!!!");
-      isPaused = true;
+      init();
     }
     if (star.x === it.x && star.y === it.y) {
       console.log("star hit body!!!");
-      isPaused = true;
-    }
-    var idx = it.y * FIELD_X + it.x;
-    if (pickStar.includes(idx)) {
-      console.log("body hit pickStar!!!");
-      console.log(String(idx) + " : " + it.x + ", " + it.y);
-      debuglog();
-      isPaused = true;
+      init();
     }
   });
   if (snake.x < 0 || snake.y < 0 || snake.x >= FIELD_X || snake.y >= FIELD_Y) {
     console.log("hit wall!!!");
-    isPaused = true;
+    init();
   }
   paint();
   if (pickStar.length == 1) isPaused = true;
@@ -484,65 +221,10 @@ const snakePaintJoint = (nextIt, itx, ity) => {
   }
 };
 
-const debuglog = () => {
-  console.log("loopCounter: ", loopCounter);
-
-  var debugbody = "";
-  var newSetSnakeBody = new Set(pickStar);
-  newSetSnakeBody.add(snake.y * FIELD_X + snake.x);
-
-  snake.body.forEach((it) => {
-    debugbody += "(" + String(it.x) + "," + String(it.y) + "), ";
-    newSetSnakeBody.add(it.y * FIELD_X + it.x);
-  });
-  console.log("snake.body: ", debugbody);
-  console.log("snake.x, snake.y: ", snake.x, snake.y);
-  console.log("newSetSnakeBody.size: ", newSetSnakeBody.size);
-
-  if (newSetSnakeBody.size != 220) {
-    isPaused = true;
-    for (let i = 0; i < 220; i++) {
-      if (!newSetSnakeBody.has(i)) {
-        console.log("miss: ", i);
-      }
-    }
-  }
-
-  // pickStarの中身をdevide個ずつ改行付きで表示
-  var devide = 100;
-  var debugpickStarDevide = "";
-  for (let i = 0; i < pickStar.length / devide; i++) {
-    debugpickStarDevide = "";
-    for (let j = 0; j < devide; j++) {
-      if (pickStar[i * devide + j] == undefined) {
-        break;
-      }
-      debugpickStarDevide += pickStar[i * 100 + j] + ", ";
-    }
-    console.log("pickStar: ", debugpickStarDevide);
-  }
-
-  console.log("pickStar.length: ", pickStar.length);
-
-  console.log("star.x, star.y: ", star.x, star.y);
-};
-
 init();
-// debugInitSP();
-
 var timerId = setInterval(function () {
   if (!isPaused) loop();
 }, SPEED);
-
-// debuglog();
-// loop();
-// debuglog();
-// loop();
-// debuglog();
-// loop();
-// debuglog();
-// loop();
-// debuglog();
 
 document.addEventListener("keydown", (e) => {
   const resultWasd = regexWasd.exec(e.code);
@@ -569,8 +251,6 @@ document.addEventListener("keydown", (e) => {
     isPaused = !isPaused;
     if (e.code == "KeyR") {
       init();
-    } else if (e.code == "KeyB") {
-      debugInitSP();
     }
   }
   direction();
